f9c3ae81237f190834c302fea92db29447451d22
Krzysztof Satola <2679192+ksatola@users.noreply.github.com>
2026-09-09 11:29:05 +0200

    feat: configure Vite API proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
