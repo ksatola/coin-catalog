c4822d689b0fa1babd6827911890dbf81d18564e
Krzysztof Satola <2679192+ksatola@users.noreply.github.com>
2026-09-14 14:23:45 +0200

    feat: add dictionaries view

---
+++ frontend/src/views/DictionariesView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DictionaryEditor from '../components/DictionaryEditor.vue'
+</script>
+
+<template>
+  <section>
+    <h1>Słowniki</h1>
+    <DictionaryEditor />
+  </section>
+</template>
