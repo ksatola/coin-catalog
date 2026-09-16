86af0f77d2d22994cf6e4b8d6236911f81a96cf6
Krzysztof Satola <2679192+ksatola@users.noreply.github.com>
2026-09-16 09:34:54 +0200

    feat: register collection API

--- backend/src/coin_catalog/main.py
+++ backend/src/coin_catalog/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from coin_catalog.routes.categories import router as categories_router
 from coin_catalog.routes.coin_categories import router as coin_categories_router
+from coin_catalog.routes.collections import router as collections_router
 from coin_catalog.routes.coins import router as coins_router
 from coin_catalog.routes.dictionaries import router as dictionaries_router
 from coin_catalog.routes.images import router as images_router
@@ -12,6 +13,7 @@ app.include_router(coins_router)
 app.include_router(dictionaries_router)
 app.include_router(categories_router)
 app.include_router(coin_categories_router)
+app.include_router(collections_router)
 app.include_router(images_router)
 
 
