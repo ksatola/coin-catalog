0cc78d126bae43fdb9fd5ba9056a220d31b89bd0
Krzysztof Satola <2679192+ksatola@users.noreply.github.com>
2026-09-16 08:59:52 +0200

    test: fix collections migration fixture

--- backend/tests/test_migration_collections.py
+++ backend/tests/test_migration_collections.py
@@ -24,7 +24,8 @@ def test_collections_migration_upgrade_and_downgrade(
         connection.execute(
             text(
                 "INSERT INTO coin (country_id, denomination_id, from_year, from_era_id, "
-                "to_year, to_era_id) VALUES (1, 1, 1900, 1, 1900, 1)"
+                "to_year, to_era_id, has_video) "
+                "VALUES (1, 1, 1900, 1, 1900, 1, 0)"
             )
         )
 
